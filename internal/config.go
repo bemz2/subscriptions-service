e82485be7ed109567af6895b3351c82e7d36dd98
Vitaliy Kharitonov <rueke23@gmail.com>
2026-04-16 23:26:29 +0300

    feat(config): добавлен конфиг-структуру с поддержкой переменных окружения и нового поля SERVER_PORT

--- internal/config.go
+++ internal/config.go
@@ -1,15 +1,34 @@
 package internal
 
+import (
+	"time"
+
+	"github.com/caarlos0/env/v11"
+	"github.com/joho/godotenv"
+)
+
 type AppConfig struct {
-	PostgresConfig PostgresConfig
+	PublicServerConfig PublicServerConfig
+	PostgresConfig     PostgresConfig
+}
+
+type PublicServerConfig struct {
+	Port string `env:"SERVER_PORT" envDefault:"1323"`
 }
 
 type PostgresConfig struct {
-	DBAdapter  string `env:"DB_ADAPTER"`
-	DBName     string `env:"POSTGRES_DB"`
-	DBHost     string `env:"POSTGRES_HOST"`
-	DBPort     string `env:"POSTGRES_PORT"`
-	DBUser     string `env:"POSTGRES_USER"`
-	DBPassword string `env:"POSTGRES_PASSWORD"`
-	DBSSLMode  string `env:"POSTGRES_SSLMODE"`
+	DBAdapter      string        `env:"DB_ADAPTER"`
+	DBName         string        `env:"POSTGRES_DB"`
+	DBHost         string        `env:"POSTGRES_HOST"`
+	DBPort         string        `env:"POSTGRES_PORT"`
+	DBUser         string        `env:"POSTGRES_USER"`
+	DBPassword     string        `env:"POSTGRES_PASSWORD"`
+	DBSSLMode      string        `env:"POSTGRES_SSLMODE"`
+	ConnectTimeout time.Duration `env:"POSTGRES_CONNECT_TIMEOUT"`
+}
+
+func NewConfig[T any](files ...string) (T, error) {
+	_ = godotenv.Overload(files...)
+	cfg := *(new(T))
+	return cfg, env.Parse(&cfg)
 }
